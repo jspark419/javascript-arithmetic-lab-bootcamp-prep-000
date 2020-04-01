0330be5d670a3b7bfa1698905b5025fe47d13ffd
John Park <jspark419@yahoo.com>
2020-04-01 20:29:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ function decrement(n) {
   return `${n - 1}`
 }
 function makeInt(n) {
-  console.log (`parses ${n} as an integer and returns the parsed integer`)
   return parseInt(`${n}`, 10)
 }
 function preserveDecimal(n) {
-  console.log ('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number')
-    return parseFloat(`${n}`)
+  return parseFloat(`${n}`)
 }
